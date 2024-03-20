a114b63574db580686f20bdaa5d75ae1180b69dc
TendyAlert <tedmanz2003@gmail.com>
2024-03-20 17:05:42 -0500

    Improved HTML formatting and styling

--- giphy-api-search.js
+++ giphy-api-search.js
@@ -1,18 +1,18 @@
 const form = document.querySelector('.js-form');
 const searchBar = document.querySelector('[name=keyword-search]');
-const submitButton = document.querySelector('.js-submit-button')
+const numberBar = document.querySelector('[name=gif-limit]');
 const imageContainer = document.querySelector('.js-image-container');
 let categories = null;
 
 async function fetchGiphyAPI() {
     const giphyAPI = await fetch('https://api.giphy.com/v1/gifs/categories?api_key=r7HXE5xtCUkN9hMp8STRaDtUMOJcui2e')
-    const response = await giphyAPI.json(); 
+    const response = await giphyAPI.json();
     categories = response.data;
     return categories;
 }
 
-async function giphySearchAPI(search) {
-    const searchAPI = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=r7HXE5xtCUkN9hMp8STRaDtUMOJcui2e&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
+async function giphySearchAPI(search, number) {
+    const searchAPI = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=r7HXE5xtCUkN9hMp8STRaDtUMOJcui2e&q=${search}&limit=${number}&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
     const response = await searchAPI.json();
     categories = response.data;
     let html = '';
@@ -23,7 +23,7 @@ async function giphySearchAPI(search) {
 function renderCategory(category) {
     return `
         <div class='category-item' id='${category.title}'>
-        <img src="${category.images.original.url}" alt="">
+        <img src="${category.images.original.url}" alt="${category.title}">
         </div> 
     `;
 }
@@ -36,9 +36,8 @@ function renderCategory(category) {
 }*/
 
 function keywordSearch(event) {
-    event.preventDefault(); 
-    giphySearchAPI(searchBar.value);
+    event.preventDefault();
+    giphySearchAPI(searchBar.value, numberBar.value);
 }
 
 form.addEventListener("submit", keywordSearch);
-submitButton.addEventListener("click", keywordSearch);
