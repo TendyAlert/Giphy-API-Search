c15149ebb8b432e546556c1c457466c87d26a13c
TendyAlert <tedmanz2003@gmail.com>
2024-03-20 14:07:24 -0500

    initial api fetch setup

--- giphy-api-search.js
+++ giphy-api-search.js
@@ -1,6 +1,14 @@
 let form = document.querySelector('.js-form');
 let searchBar = document.querySelector('[name=keyword-search]');
 let imageContainer = document.querySelector('.js-image-container');
+let categories = null;
+
+async function fetchGiphyAPI() {
+    const giphyAPI = await fetch('https://api.giphy.com/v1/gifs/categories?api_key=r7HXE5xtCUkN9hMp8STRaDtUMOJcui2e')
+    const response = await giphyAPI.json(); 
+    categories = response.data;
+    console.log(categories);
+}
 
 
 
